e7971e8dbe1887943fe505d2ec3ce377c286702e
Nick Jason Hagen <nickjasonhagen@gmail.com>
2026-02-23 21:12:06 +0100

    Line trait , executableblock

--- src/nscriptengine/core.rs
+++ src/nscriptengine/core.rs
@@ -8,3 +8,12 @@ pub fn new()->Nscript{
         Nscript{}
     }
 }
+// each line instruction for execution.
+pub trait NscriptLine{
+    fn exec(&mut self) ->Option<NscriptVariable>;
+}
+// holds the tokenized code which the interpreter uses to exectue
+pub struct NscriptExecutableBlock{
+    pub codevec:Vec<Box<dyn NscriptLine>>
+}
+
