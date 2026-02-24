b98ee796f0b6c2c60fee6613664a41dbc1f01dd9
Nick Jason Hagen <nickjasonhagen@gmail.com>
2026-02-24 20:53:08 +0100

    broken, gotta check generics over traits for this one

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ mod nscriptengine {
     pub mod types;
 
 }
+pub const VERSION: &'static str  = "4.0.001";
 pub use nscriptengine::core::*;
 pub use nscriptengine::parser::*;
 pub use nscriptengine::storage::*;
 pub use nscriptengine::types::*;
 pub use std::collections::HashMap;
+pub use std::clone::Clone;
 fn test_nscriptvariable() {
     let  vars = vec!(NscriptVariable::newstring("test".to_string()),NscriptVariable::newfloat(0.0),NscriptVariable::newfloat(10.123123),NscriptVariable::newint(100));
     for mut xvar in vars{
--- src/nscriptengine/types.rs
+++ src/nscriptengine/types.rs
@@ -1,42 +1,53 @@
 use crate::*;
 
+// Variable type used in nscriptcode, holds a trait NscriptValue
+
 pub struct NscriptVariable{
     pub value: Box<dyn NscriptValue>,
     pub vec:Vec<NscriptVariable>
 }
 impl NscriptVariable{
-pub fn new()->NscriptVariable{
+    pub fn clone(&self)->NscriptVariable{
+        let mut vec = Vec::new();
+        for x in self.vec{
+            vec.push(self.value.clone());
+        }
+        NscriptVariable{value:self.value.clone(),vec:self.vec}
+    }
+    pub fn new()->NscriptVariable{
         NscriptVariable{
             value: Box::new(NscriptString::new("".to_string())),
             vec: Vec::new(),
         }
     }
     pub fn newstring(data:String)->NscriptVariable{
-        let d = NscriptString{
-            value:data,
-        };
+        let d = NscriptString::new(data);
         NscriptVariable{value:Box::new(d),vec:Vec::new()}
     }
     pub fn newint(data:u64)->NscriptVariable{
-        let d = NscriptInt{
-            value:data,
-        };
+        let d = NscriptInt::new(data);
         NscriptVariable{value:Box::new(d),vec:Vec::new()}
     }
     pub fn newfloat(data:f64)->NscriptVariable{
-        let d = NscriptFloat{
-            value:data,
-        };
+        let d = NscriptFloat::new(data);
         NscriptVariable{value:Box::new(d),vec:Vec::new()}
     }
+
 }
+
 // trait for a value, allows you to add your own.
 pub trait NscriptValue{
+
+    fn clone<T>(T:&self) ->T;
     fn string(&mut self)->String;
     fn int(&mut self)->u64;
     fn float(&mut self)->f64;
+    fn object(&mut self)->Option<NscriptClass>;
 }
-struct NscriptString{
+
+// default NscriptValue for holding strings
+#[derive(Clone)]
+pub struct NscriptString{
     value:String,
 }
 impl NscriptString{
@@ -45,8 +56,14 @@ impl NscriptString{
             value:string,
         }
     }
+
 }
 impl NscriptValue for NscriptString{
+    fn clone(&self) ->Self{
+        NscriptString{
+            value:self.value.clone()
+        }
+    }
     fn string(&mut self)->String{
         return self.value.to_string();
     }
@@ -56,9 +73,14 @@ impl NscriptValue for NscriptString{
     fn float(&mut self)->f64{
         return self.value.parse::<f64>().unwrap_or(0.0);
     }
+    fn object(&mut self)->Option<NscriptClass>{
+        return None;
+    }
 }
 
-struct NscriptInt{
+// default NscriptValue for holding int
+#[derive(Clone)]
+pub struct NscriptInt{
     value:u64,
 }
 impl NscriptInt{
@@ -69,6 +91,11 @@ impl NscriptInt{
     }
 }
 impl NscriptValue for NscriptInt{
+    fn clone(&self) ->Self{
+        NscriptInt{
+            value:self.value.clone()
+        }
+    }
     fn string(&mut self)->String{
         return self.value.to_string();
     }
@@ -78,9 +105,14 @@ impl NscriptValue for NscriptInt{
     fn float(&mut self)->f64{
         return self.value as f64;
     }
+    fn object(&mut self)->Option<NscriptClass>{
+        return None;
+    }
 }
 
 
+// default NscriptValue for holding float
+//#[derive(Clone)]
 struct NscriptFloat{
     value:f64,
 }
@@ -92,6 +124,11 @@ impl NscriptFloat{
     }
 }
 impl NscriptValue for NscriptFloat{
+    fn clone(&self) ->Self{
+        NscriptFloat{
+            value:self.value.clone()
+        }
+    }
     fn string(&mut self)->String{
         return self.value.to_string();
     }
@@ -100,5 +137,33 @@ impl NscriptValue for NscriptFloat{
     }
     fn float(&mut self)->f64{
         return self.value.clone();
+    }
+    fn object(&mut self)->Option<NscriptClass>{
+        return None;
+    }
+}
+//#[derive(Clone)]
+struct NscriptClass{
+    value:String,
+    variables:HashMap<Box<str>,NscriptVariable>,
+}
+impl NscriptValue for NscriptClass{
+    fn clone(&self) ->Self{
+        NscriptClass{
+            value:self.value.clone(),
+            variables:self.variables.clone()
+        }
+    }
+    fn string(&mut self)->String{
+        return self.value.to_string();
+    }
+    fn int(&mut self)->u64{
+        return 0;
+    }
+    fn float(&mut self)->f64{
+        return 0.0;
+    }
+    fn object(&mut self)->Option<NscriptClass>{
+        return Some(self.clone());
     }
 }
